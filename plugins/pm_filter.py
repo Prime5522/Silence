c2c0d7c0bc106f31991cdf5c452d90117e2003cb
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-06-11 22:50:10 +0600

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1883,7 +1883,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     
 async def auto_filter(client, msg, spoll=False):
-    curr_time = datetime.now(pytz.timezone('Asia/Kolkata')).time()
+    bd_timee = message.date + timedelta(hours=6)
 
     if not spoll:
         message = msg
@@ -1943,7 +1943,7 @@ async def auto_filter(client, msg, spoll=False):
     f"🎬 **ꜰɪʟᴇ ɴᴀᴍᴇ:** `{search}`\n"
     f"🆔 **ᴜsᴇʀ ɪᴅ:** [ᴠɪᴇᴡ ᴩʀᴏꜰɪʟᴇ](tg://openmessage?user_id={message.from_user.id})\n"
     f"👤 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ:** `{message.from_user.first_name}`\n"
-    f"⏰ **ʀᴇǫᴜᴇsᴛᴇᴅ ᴏɴ:** `{datetime.now().strftime('%d %B %Y, %I:%M %p')}`\n"
+    f"⏰ **ʀᴇǫᴜᴇsᴛᴇᴅ ᴏɴ:** <code>{bd_timee.strftime('%Y-%m-%d %I:%M:%S %p')} (BST)</code>\n"
     f"💌 **sᴛᴀᴛᴜs:** ᴩᴇɴᴅɪɴɢ 🔄\n", 
     reply_markup = InlineKeyboardMarkup([
     # ✅ বড় বোতাম - Uploaded Done
